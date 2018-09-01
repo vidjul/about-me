f29395037901664a2f81e5c9689686ec7c8c2a51
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-09-01 23:42:34 +0200

    feat(layout):  premier jet pour remplacer la page par défaut de React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header></header>
+        <section className="App-intro">
+          <h1> Hey !</h1>
+          <p> Je m'appelle Vidushan CHOORIYAKUMARAN. </p>
+          <p>
+            Je suis actuellement un étudiant en cinquième année à l'ESILV.
+            Le site est actuellement en développement, pourquoi ne pas revenir plus tard ? ;) <br />
+            En attendant, je vous invite à me contacter à travers mes réseaux sociaux.
+          </p>
+        </section>
+        <section className="App-social">
+          <a href="https://www.linkedin.com/in/c-vidushan/"><i class="fab fa-linkedin-in fa-6x"></i></a>
+          <a href="https://www.facebook.com/vidushan.chooriyakumaran"><i class="fab fa-facebook-f fa-6x"></i></a>
+          <a href="https://www.github.com/vidjul"><i class="fab fa-github fa-6x"></i></a>
+        </section>
       </div>
     );
   }
